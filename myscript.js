b10b26f11558a86fe919638f1e7f70e9e285d3be
sharpar <shivain27@gmail.com>
2016-02-04 20:40:36 -0500

    javascript finally working

--- myscript.js
+++ myscript.js
@@ -1,15 +1,17 @@
-//$(document).ready(getQuote);
-
-
 $(document).ready(function () {
-    $('.icon').fadeIn('slow');
-});
 
-quoteArray = [
+    var quoteArray = [
 ["A lot of late nights in the gym, a lot of early mornings, especially when your friends are going out, you're going to the gym, those are the sacrifices that you have to make if you want to be an NBA basketball player.", "Jason Kidd"],
-["I look at my jersey and see the NBA logo, I'm like, 'I didn't think I'd be here.", "Kevin Durant"]];
+["I look at my jersey and see the NBA logo, I'm like, 'I didn't think I'd be here.", "Kevin Durant"],
+["I knew it was time to retire when I was driving down the lane and got called for a three-second violation.", " Johnny Kerr, former NBA player and coach "]
+            ];
 
 
-$(".generate").click(function () {
-    $('.quote').text(quoteArray[0][0]);
-})
+    $(".generate").click(function () {
+
+        var num = Math.floor(Math.random() * quoteArray.length) + 1;
+
+        $('.quote').text(quoteArray[num][0]);
+    });
+
+});
